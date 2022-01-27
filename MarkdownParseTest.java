7ba2cdcb198b72ac7ee06a0d93afaa7149e8b4a8
kkbunny247 <kennnyclaire@gmail.com>
2022-01-27 14:50:15 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -13,11 +13,35 @@ public class MarkdownParseTest {
     }
 
     @Test
-    public void getLinks() throws IOException {
+    public void getLinks1() throws IOException {
         Path fileName = Path.of("test-file.md");
 	    String contents = Files.readString(fileName);
 
         assertEquals(MarkdownParse.getLinks(contents), List.of("https://something.com", "some-page.html"));
+    }
+
+    @Test
+    public void getLinks2() throws IOException {
+        Path fileName = Path.of("test2-file.md");
+	    String contents = Files.readString(fileName);
+
+        assertEquals(MarkdownParse.getLinks(contents), List.of("link.com"));
+    } 
+
+    @Test
+    public void getLinks3() throws IOException {
+        Path fileName = Path.of("test3-file.md");
+	    String contents = Files.readString(fileName);
+
+        assertEquals(MarkdownParse.getLinks(contents), List.of(""));
+    } 
+
+    @Test
+    public void getLinks4() throws IOException {
+        Path fileName = Path.of("test4-file.md");
+	    String contents = Files.readString(fileName);
+
+        assertEquals(MarkdownParse.getLinks(contents), List.of());
     } 
 }
 
