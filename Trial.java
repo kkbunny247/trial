f5cebf6eb6d372da1f143a42d1948d9acbe62218
kkbunny247 <kennnyclaire@gmail.com>
2022-02-15 13:01:58 -0800

    ok

--- Trial.java
+++ Trial.java
@@ -1,5 +1,44 @@
+// File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
 public class Trial {
-    public static int mod(int x, int y) {
-      return (x % y);
+  public static ArrayList<String> getLinks(String markdown) {
+    ArrayList<String> toReturn = new ArrayList<>();
+    // find the next [, then find the ], then find the (, then take up to
+    // the next )
+    int currentIndex = 0;
+    while(currentIndex < markdown.length()) {
+        int nextOpenBracket = markdown.indexOf("[", currentIndex);
+        System.out.format("%d\t%d\t%s\n", currentIndex, nextOpenBracket, toReturn);
+        int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
+        int openParen = markdown.indexOf("(", nextCloseBracket);
+        int closeParen = markdown.indexOf(")", openParen);
+        if(nextOpenBracket == -1 || nextCloseBracket == -1
+              || closeParen == -1 || openParen == -1) {
+                System.out.println(currentIndex + "last line");
+            return toReturn;
+        }
+        String potentialLink = markdown.substring(openParen + 1, closeParen);
+        if(potentialLink.indexOf(" ") == -1 && potentialLink.indexOf("\r\n") == -1) {
+          System.out.println(currentIndex);
+            toReturn.add(potentialLink);
+            currentIndex = closeParen + 1;
+            System.out.println(currentIndex);
+        }
+        else {
+          System.out.println(currentIndex);
+            currentIndex = currentIndex + 1;
+        }
     }
+    return toReturn;
+  }
+  public static void main(String[] args) throws IOException {
+  Path fileName = Path.of(args[0]);
+    String contents = Files.readString(fileName);
+      ArrayList<String> links = getLinks(contents);
+      System.out.println(links);
+  }
 }
